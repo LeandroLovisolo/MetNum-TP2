5147824e7652978b4bea8d7899237c55f60f1fd7
Lautaro Petaccio <lausuper@gmail.com>
2013-05-08 00:02:25 -0300

    Agregada funcion para leer señales de sonido

--- Metnum-TP2.cpp
+++ Metnum-TP2.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <fstream>
 #include "Matriz.h"
 #include "Ecuaciones.h"
 using namespace std;
 
+Matriz *cargarSonido(char *fileName) {
+	Matriz *mat;
+	ifstream file(fileName);
+	if(file.is_open()) {
+		cout << "Abrio el archivo" << endl;
+		int longitud;
+		file >> longitud;
+		mat = new Matriz(longitud, 1);
+		for(int i=0;i<longitud;i++) {
+			file >> mat->elem(i,0);
+		}
+		file.close();
+	}
+	return mat;
+}
+
 int main() {
-	Matriz *mat = MatrizM(5, 10);
+	Matriz *mat = cargarSonido("signals/s64.txt");
 	mat->print();
 	return 0;
 }
