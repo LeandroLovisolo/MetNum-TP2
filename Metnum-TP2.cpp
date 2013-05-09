e905987ff6d9bcf6d99060a17d97f102d24e9952
Lautaro Petaccio <lausuper@gmail.com>
2013-05-09 10:51:52 -0300

    Cambiados parámetros de PSNR y ECM a punteros

--- Metnum-TP2.cpp
+++ Metnum-TP2.cpp
@@ -45,7 +45,9 @@ int main() {
 	plu.elem(3,1) = 2;
 	plu.elem(3,2) = 3;
 	plu.elem(3,3) = -1;
-	
+
+
+	/*
 	cout << "Matriz Original: " << endl;
 	pair<Matriz*,Matriz*> res = plu.factorizacionPLU();
 	cout << "Matriz P: " << endl;
@@ -55,7 +57,7 @@ int main() {
 	cout << "Tratando de re-contruir A = P.L.U" << endl;
 	(*res.first)*(*res.second); //Multiplico P por L
 	(*res.first)*plu; //Multiplico PL por U
-	res.first->print();
+	res.first->print();*/
 	/*
 	cout << "Matriz Original: " << endl;
 	Matriz *L = plu.factorizacionLU();
