52b7c725244c10c0775556f4dec69817a3c44b54
Leandro Lovisolo <leandro@leandro.me>
2013-05-10 13:31:47 -0300

    Formatting

--- Ecuaciones.h
+++ Ecuaciones.h
@@ -2,16 +2,16 @@
 #define ECUACIONES_H_
 #include "Matriz.h"
 
-Matriz *MatrizG(const int n);
-Matriz *MatrizSt(const int n);
+Matriz* MatrizG(const int n);
+Matriz* MatrizSt(const int n);
 double C(const int k, const int n);
-Matriz *MatrizT(const int n);
-Matriz *MatrizM(const int n, const int rango);
-Matriz *MatrizMsombrero(const int n);
-Matriz *aplicarDCT(Matriz *x);
-Matriz *revertirDCT(Matriz *x, const int rango);
+Matriz* MatrizT(const int n);
+Matriz* MatrizM(const int n, const int rango);
+Matriz* MatrizMsombrero(const int n);
+Matriz* aplicarDCT(Matriz& x);
+Matriz* revertirDCT(Matriz& x, const int rango);
 
-double PSNR(Matriz *matOriginal, Matriz *matPerturbada, const int rangoMax);
-double ECM(Matriz *matOriginal, Matriz *matPerturbada);
+double PSNR(Matriz* matOriginal, Matriz* matPerturbada, const int rangoMax);
+double ECM(Matriz* matOriginal, Matriz* matPerturbada);
 
 #endif /* ECUACIONES_H_ */
