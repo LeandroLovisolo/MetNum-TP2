f44f110a88b15f3ee0692c7bc297b08159ba7b50
Leandro Lovisolo <leandro@leandro.me>
2013-05-16 23:34:22 -0300

    Bugfix en PSNR

--- octave/psnr.m
+++ octave/psnr.m
@@ -1,4 +1,7 @@
 function res = psnr(original, con_ruido)
 
+original  = double(original);
+con_ruido = double(con_ruido);
+
 ecm = sum((original - con_ruido).^2) / length(original);
 res = 10 * log10(max(original)^2 / ecm);
