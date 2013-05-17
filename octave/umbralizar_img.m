4231a9c410b05c80c460f317b5e58b8ae57553d8
Leandro Lovisolo <leandro@leandro.me>
2013-05-17 02:57:05 -0300

    Umbralizar imagen

--- octave/umbralizar_img.m
+++ octave/umbralizar_img.m
@@ -1,14 +1,38 @@
-function out = umbralizar_img(B, umbral)
+function out = umbralizar_img(B, coefInicial, coefFinal, umbral)
 
 D = imagen_a_dct(B);
 
-for i = 1:length(D)
-    for j = 1:length(D)
-        if(abs(D(i,j)) <= umbral)
-            D(i,j) = 0;
-            printf('Eliminando coeficiente (%d, %d)\n', i, j);
+% El par (x0, y0) indica el primer elemento de la diagonal actual
+x0 = 1;
+y0 = 1;
+
+% El par (x, y) indica el coeficiente actual
+x = x0;
+y = y0;
+
+for i = 1:length(B)^2
+    % Umbralizo coeficiente actual
+    if(coefInicial <= i && i <= coefFinal)
+       if(abs(D(y, x)) < umbral); D(y, x) = 0; endif;
+    endif;
+
+    % Avanzo diagonalmente al siguiente coeficiente
+    x--;
+    y++;
+
+    % Verifico si llegué al final de la diagonal actual
+    if(x < 1 || y > length(B))
+        % Avanzo a la siguiente diagonal
+        if(x0 < length(B))
+            x0++;
+        else
+            y0++;
         endif
-    end
+
+        % Avanzo al primer elemento de la nueva diagonal
+        x = x0;
+        y = y0;
+    endif
 end
 
 out = dct_a_imagen(D);
